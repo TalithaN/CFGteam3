e4415689844bf074965f8e17767649655035a4b2
Claudia <claubituna@gmail.com>
2023-06-11 19:42:16 +0200

    reviews part added

--- newsletter.js
+++ newsletter.js
@@ -6,3 +6,20 @@ showForm.addEventListener('submit', (e) => {
     e.preventDefault();
     favDialog.showModal();
   });
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
